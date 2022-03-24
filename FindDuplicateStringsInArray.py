ae6121d278a04ba8614bc3ad21155bf08e57f4e6
Sridhar_Dachepelly <yodacda@gmail.com>
2022-03-24 00:17:10 -0500

    Commiting second version

--- FindDuplicateStringsInArray.py
+++ FindDuplicateStringsInArray.py
@@ -1,9 +1,7 @@
 names = ["Sridhar", "Usha", "Nishkal", "Anvika", "Sridhar", "Robert", "Susan", "Samba", "Usha", "Alex", "Andy"]
 
 for i in range(0, len(names)):
-    for j in range(i+1, len(names)):
+    for j in range(i + 1, len(names)):
         if names[i] == names[j]:
-           print(names[i])
-           break
-
-
+            print(names[i])
+            break
---
+++ PathPackage.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+#path = Path("ecommerce")
+#print(path.rmdir())
+
+path1 = Path()
+for file in path1.glob("*"):
+    print(file)
+
